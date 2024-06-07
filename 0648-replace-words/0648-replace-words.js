4460e2bad2f693617aa005e9f6ec9808b467b75b
Debasish Das <102917771+Debasish728@users.noreply.github.com>
2024-06-07 18:16:42 +0530

    Time: 400 ms (40.5%), Space: 60.1 MB (74.79%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+const replaceWords = function(dictionary, sentence) {
+     let arr  = sentence.split(" ");
+    for(let i=0; i< arr.length; i++){
+        for(let j=0; j< dictionary.length; j++){
+            if(dictionary[j] && arr[i].startsWith(dictionary[j])){
+                arr[i] = dictionary[j];
+            }
+        }
+    }
+
+    return arr.join(" ");
+};
