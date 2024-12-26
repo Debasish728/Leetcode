15eb73e93d4757e96e7b05c1cb25eb8fc9ba272a
Debasish Das <102917771+Debasish728@users.noreply.github.com>
2024-12-26 13:15:37 +0530

    Time: 134 ms (40.64%), Space: 45.9 MB (5.48%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-    int helper(int[] nums , int i , int target , int len ,int sum ){
+    int helper(int[] nums , int i , int target , int len ,int sum , HashMap<String , Integer> map ){
         if(i==len-1){
             if(sum == target){
                 return 1;
@@ -7,18 +7,24 @@ class Solution {
                 return 0;
             }
         }
+        String key = String.valueOf(i)+'_'+String.valueOf(sum);
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
 
-        int add = helper(nums , i+1 , target , len , sum+nums[i+1] );
-        int sub = helper(nums , i+1 , target , len , sum-nums[i+1] );
-        return add+sub;
+        int add = helper(nums , i+1 , target , len , sum+nums[i+1] , map );
+        int sub = helper(nums , i+1 , target , len , sum-nums[i+1] , map );
+        map.put(key , add+sub);
+        return  add+sub;
 
     }
 
     public int findTargetSumWays(int[] nums, int target) {
         int len = nums.length;
+        HashMap<String , Integer> map = new HashMap<>();
        
         
-        return helper(nums ,0 , target , len , nums[0]) + helper(nums , 0 , target , len , -nums[0]) ;
+        return helper(nums ,0 , target , len , nums[0] , map) + helper(nums , 0 , target , len , -nums[0] , map) ;
     
         
 
