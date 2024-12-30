94e7388159e66cdd371373aef9990570e3ef5620
Debasish Das <102917771+Debasish728@users.noreply.github.com>
2024-12-30 12:47:20 +0530

    Time: 12 ms (31.54%), Space: 43.5 MB (68.12%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -1,32 +1,23 @@
 class Solution {
-    int l , h , z , o;
-    int Mod = 1000000007;
-    int solve(int len , int memo[]){
-        if(len>h){
-            return 0;
+    public int countGoodStrings(int low, int high, int zero, int one) {
+        int dp[] = new int [high+1];
+        dp[0] = 1;
+        int Mod = 1000000007;
+        for(int i=1;i<=high;i++){
+            if(i-zero>=0){
+                dp[i] = (dp[i]%Mod + dp[i-zero]%Mod)%Mod;
+            }
+            if(i-one>=0){
+                dp[i] = (dp[i]%Mod+dp[i-one]%Mod)%Mod;
+            }
+            
         }
-        if(memo[len] != -1){
-            return memo[len];
-        }
-        boolean addOne = false;
-        if(len>=l){
-            addOne = true;
-        }
-        int takeZero = solve(len+z , memo);
-        int takeOne = solve(len+o , memo);
-        memo[len] =  ((addOne ? 1:0)+takeZero+takeOne);
-        memo[len] = memo[len]%Mod;
-        return memo[len];
+        int res=0;
+        for(int i=low;i<=high;i++){
+            res= (res%Mod + dp[i]%Mod)%Mod;
 
         }
-    public int countGoodStrings(int low, int high, int zero, int one) {
-        l = low;
-        h = high ;
-        z = zero;
-        o = one;
-        int memo[] = new int[high+1];
-        Arrays.fill(memo , -1);
-        return solve(0,memo);
+        return res;
         
     }
 }
